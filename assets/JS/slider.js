dd286887699fba3f593b3702f9bc93b0d4332500
shrish karki <shrishkarki6669@gmail.com>
2025-05-16 14:44:30 +0545

    added stats section

--- assets/JS/slider.js
+++ assets/JS/slider.js
@@ -1,9 +1,9 @@
 const list=document.querySelector(".testimonials__list");
 const cards=document.querySelector(".testimonials__card");
 const dots=document.querySelectorAll(".testimonials__slider-btn");
-const card = document.querySelector(".testimonials__card");
+let card = document.querySelector(".testimonials__card");
  
-let cardWidth=null;
+let cardWidth=card.offsetWidth;
 
 window.addEventListener("resize",()=>{
     if(card){
@@ -17,14 +17,17 @@ window.addEventListener("resize",()=>{
 
 function goToSlide(index) {
     list.style.transform = `translateX(-${index * cardWidth}px)`;
+    
     //  list.style.transform = `translateX(-690px)`;
     dots.forEach(dot => dot.classList.remove("active"));
     dots[index].classList.add("active");
   }
     dots.forEach(dot => {
     dot.addEventListener("click", () => {
+        console.log("clickeddddd")
       const index = parseInt(dot.dataset.index, 10);
+      console.log("index",index)
       goToSlide(index);
     });
   });
-  goToSlide(0)
+  goToSlide(0);
