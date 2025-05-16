fee12c36f57ab3a6d022659bd760e7ade789bb26
shrish karki <shrishkarki6669@gmail.com>
2025-05-16 13:35:38 +0545

    slider responsive

--- assets/JS/slider.js
+++ assets/JS/slider.js
@@ -2,17 +2,21 @@ const list=document.querySelector(".testimonials__list");
 const cards=document.querySelector(".testimonials__card");
 const dots=document.querySelectorAll(".testimonials__slider-btn");
 const card = document.querySelector(".testimonials__card");
-// console.log("cardwidth",cardWidth); 
-
+ 
+let cardWidth=null;
 
 window.addEventListener("resize",()=>{
-if(card){
-     const cardWidth = card.offsetWidth;
-    console.log("Card width:", cardWidth);
-}
+    if(card){
+        cardWidth = card.offsetWidth;
+        
+    }
 })
+
+// console.log("Card width:", cardWidth);
+
+
 function goToSlide(index) {
-    list.style.transform = `translateX(-${index * 69}rem)`;
+    list.style.transform = `translateX(-${index * cardWidth}px)`;
     //  list.style.transform = `translateX(-690px)`;
     dots.forEach(dot => dot.classList.remove("active"));
     dots[index].classList.add("active");
